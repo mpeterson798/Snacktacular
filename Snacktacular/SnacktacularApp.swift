972e349606b607764b2a4ba37cd2d36048050d5b
Matthew Peterson <matthew251125@icloud.com>
2025-12-28 12:36:15 -0600

    Initial Commit

---
+++ Snacktacular/SnacktacularApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularApp.swift
+//  Snacktacular
+//
+//  Created by Matthew Peterson on 12/28/25.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
