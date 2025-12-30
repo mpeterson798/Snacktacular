59f284cabe9877230aeedaf8237b13035f301ec5
Matthew Peterson <matthew251125@icloud.com>
2025-12-30 15:00:38 -0600

    Photos save

--- Snacktacular/Models/Photo.swift
+++ Snacktacular/Models/Photo.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import FirebaseAuth
+
+class Photo: Identifiable, Codable {
+    @DocumentID var id: String?
+    var imageURLString = "" // This will hold the URL for loading the image
+    var description = ""
+    var reviewer: String = Auth.auth().currentUser?.email ?? ""
+    var postedOn = Date() // current date/time
+}
